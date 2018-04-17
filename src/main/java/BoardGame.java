6dd74c1d73b251f04490d98e86f44632fadca74f
Paula Bonastre <bonastrep@gmail.com>
2018-04-17 13:24:30 +0200

    add main

--- src/main/java/BoardGame.java
+++ src/main/java/BoardGame.java
@@ -1,2 +1,15 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class BoardGame {
+
+    private static final String INFILENAME = "C:\\workSpaceIJ\\input\\inputChallenge3.txt";
+    private static String OUTFILENAME =  "C:\\workSpaceIJ\\input\\outputChallenge3.txt";
+    private static Scanner in;
+    private static PrintWriter out;
+
+    public static void main(String[] args) {
+
+    }
+
 }
